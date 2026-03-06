ae2c4146c2a94c89a8b71eaef57e1b24062f97f7
Krista Mulder <kristamulder@Kristas-MacBook-Air-3.local>
2026-03-06 15:22:04 -0700

    Updated

--- app/src/main/java/com/example/lab08/City.java
+++ app/src/main/java/com/example/lab08/City.java
@@ -4,6 +4,11 @@ public class City {
     private String city;
     private String province;
 
+    /**
+     *
+     * @param city
+     * @param province
+     */
     public City(String city, String province){
         this.city = city;
         this.province = province;
